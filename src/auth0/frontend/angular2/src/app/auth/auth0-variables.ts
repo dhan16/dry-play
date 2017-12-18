0b50141916fc0dc2fe3a4d8effcd5b9a8fee3451
CDN MacbookPro13 <chandan.nath@gmail.com>
2017-12-18 23:01:55 +0000

    add auth

--- src/auth0/frontend/angular2/src/app/auth/auth0-variables.ts
+++ src/auth0/frontend/angular2/src/app/auth/auth0-variables.ts
@@ -2,10 +2,12 @@ interface AuthConfig {
   clientID: string;
   domain: string;
   callbackURL: string;  // also used as logoutReturnToURL
+  audience: string;
 }
 
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'LejDX0O6TCLHmpj6tM9CeufbIlhsbXsv',
   domain: 'dhan16.eu.auth0.com',
-  callbackURL: 'http://localhost:4200'
+  callbackURL: 'http://localhost:4200',
+  audience: 'https://dhan16-api1.com',
 };
