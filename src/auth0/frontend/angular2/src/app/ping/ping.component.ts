6739a371d37d55929b6439fe00cb6f5226b729f3
CDN MacbookPro13 <chandan.nath@gmail.com>
2017-12-19 23:59:40 +0000

    match API to backend and log res

--- src/auth0/frontend/angular2/src/app/ping/ping.component.ts
+++ src/auth0/frontend/angular2/src/app/ping/ping.component.ts
@@ -11,7 +11,7 @@ import 'rxjs/add/operator/map';
 })
 export class PingComponent implements OnInit {
 
-  API_URL = 'http://localhost:3001/api';
+  API_URL = 'http://localhost:3010/api';
   message: string;
 
   constructor(public auth: AuthService, public http: Http, public authHttp: AuthHttp) {}
@@ -21,21 +21,21 @@ export class PingComponent implements OnInit {
 
   public ping(): void {
     this.message = '';
-    this.http.get(`${this.API_URL}/public`)
-      .map(res => res.json())
+    this.http.get(`${this.API_URL}/public/`)
+      .map(res => {console.log(res); return res.text(); })
       .subscribe(
-        data => this.message = data.message,
+        message => this.message = message,
         error => this.message = error
       );
   }
 
   public securedPing(): void {
     this.message = '';
-    this.authHttp.get(`${this.API_URL}/private`)
-      .map(res => res.json())
+    this.authHttp.get(`${this.API_URL}/private/`)
+      .map(res => {console.log(res); return res.json(); })
       .subscribe(
-        data => this.message = data.message,
-        error => this.message = error
+        data => {console.log(data); this.message = data.message; },
+        error => {console.log(error); this.message = error; },
       );
   }
 }
