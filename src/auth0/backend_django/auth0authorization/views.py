6709b7f007b1ce43a9daf462ddbe7c4ba173c27f
CDN MacbookPro13 <chandan.nath@gmail.com>
2017-12-20 23:26:24 +0000

    move from views.py

--- src/auth0/backend_django/auth0authorization/views.py
+++ src/auth0/backend_django/auth0authorization/views.py
@@ -1,54 +1,6 @@
-import jwt
-import json
-from functools import wraps
-
-from django.conf import settings
-from django.http import HttpResponse
 from rest_framework.decorators import api_view
-from six.moves.urllib import request as req
-from cryptography.x509 import load_pem_x509_certificate
-from cryptography.hazmat.backends import default_backend
 
-# Create your views here.
-
-
-def get_token_auth_header(request):
-    """Obtains the access token from the Authorization Header
-    """
-    auth = request.META.get("HTTP_AUTHORIZATION", None)
-    parts = auth.split()
-    token = parts[1]
-
-    return token
-
-
-def requires_scope(required_scope):
-    """Determines if the required scope is present in the access token
-    Args:
-        required_scope (str): The scope required to access the resource
-    """
-    def require_scope(f):
-        @wraps(f)
-        def decorated(*args, **kwargs):
-            token = get_token_auth_header(args[0])
-            AUTH0_DOMAIN = settings.AUTH0_DOMAIN
-            API_IDENTIFIER = settings.API_IDENTIFIER
-            jsonurl = req.urlopen('https://' + AUTH0_DOMAIN + '/.well-known/jwks.json')
-            jwks = json.loads(jsonurl.read())
-            cert = '-----BEGIN CERTIFICATE-----\n' + jwks['keys'][0]['x5c'][0] + '\n-----END CERTIFICATE-----'
-            certificate = load_pem_x509_certificate(cert.encode('utf-8'), default_backend())
-            public_key = certificate.public_key()
-            decoded = jwt.decode(token, public_key, audience=API_IDENTIFIER, algorithms=['RS256'])
-
-            if decoded.get("scope"):
-                token_scopes = decoded["scope"].split()
-                for token_scope in token_scopes:
-                    if token_scope == required_scope:
-                        return f(*args, **kwargs)
-            return HttpResponse("You don't have access to this resource")
-        return decorated
-    return require_scope
-
+from auth0authorization.auth import *
 
 def public(request):
     return HttpResponse("All good. You don't need to be authenticated to call this")
@@ -64,7 +16,9 @@ def private(request):
 def private_read_messages(request):
     return HttpResponse("All good. You're authenticated and can read messages")
 
+
 @api_view(['GET'])
-@requires_scope('read:group_messages')
+@requires_scope_and_group('read:group_messages')
 def private_read_groupmessages(request):
-    return HttpResponse("All good. You're authenticated and can read group messages")
+    return HttpResponse("All good. You're authenticated and can read messages for the groups:%s"
+                        % ' '.join(user_groups(request)))
