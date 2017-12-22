660732f019194de5688c71b6d60ebe60e1155491
CDN MacbookPro13 <chandan.nath@gmail.com>
2017-12-22 12:33:52 +0000

    remove unnecessary stuff

--- src/auth0/backend_django/apiexample/settings.py
+++ src/auth0/backend_django/apiexample/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework'
 ]
 
@@ -90,10 +89,6 @@ DATABASES = {
     }
 }
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
