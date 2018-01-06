866e3d8c6bc2f899fb00b67aaf171f05fc100fa8
CDN MacbookPro13 <chandan.nath@gmail.com>
2018-01-06 10:29:35 +0000

    make PEP compliant

--- src/python/examples/helloworld/ml_classifier.py
+++ src/python/examples/helloworld/ml_classifier.py
@@ -1,8 +1,9 @@
-'''
+"""
 https://www.quora.com/Who-can-show-me-a-piece-of-machine-learning-code-and-explain-in-short-what-it-does
-'''
-import numpy as np #library for basic matrix calculation
-import matplotlib.pyplot as plt #library for basic plotting
+"""
+
+import numpy as np  # library for basic matrix calculation
+import matplotlib.pyplot as plt  # library for basic plotting
 
 
 def min_distance(group, point):
@@ -11,15 +12,15 @@ def min_distance(group, point):
 
 
 N = 5
-stars = np.random.rand(2,N)
-triangles = .5 + np.random.rand(2,N)
+stars = np.random.rand(2, N)
+triangles = .5 + np.random.rand(2, N)
 
 plt.scatter(stars[0], stars[1], c='r', marker='*', s=100)
 plt.scatter(triangles[0], triangles[1], c='b', marker='^', s=100)
 plt.show()
 
-new_point = np.random.rand(2,1)
-plt.scatter(new_point[0], new_point[1],c='k',marker='o', s=100)
+new_point = np.random.rand(2, 1)
+plt.scatter(new_point[0], new_point[1], c='k', marker='o', s=100)
 plt.show()
 
 cluster = 'stars' if min_distance(stars, new_point) < min_distance(triangles, new_point) else 'triangles'
