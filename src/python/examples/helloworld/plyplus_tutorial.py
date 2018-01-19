8d45d71e723728c77d731309bedd9509e3384313
CDN MacbookPro13 <chandan.nath@gmail.com>
2018-01-19 16:41:55 +0000

    keyword can have multiple data

--- src/python/examples/helloworld/plyplus_tutorial.py
+++ src/python/examples/helloworld/plyplus_tutorial.py
@@ -5,15 +5,15 @@ parser1 = Grammar(r"start: name ('\s*,' name)* ; name: '\w+' ;")
 r=parser1.parse('cat ,milk,dog')
 
 parser2 = Grammar("""
-    start: (section_rule)+ 'END' ;
-    section_rule: section_token section_data ;
-    section_token: 'RUNSPEC' | 'SCHEDULE' ;
-    section_data: (keyword_rule)+ ;
-    @keyword_rule: nodata_token | data_keyword_rule ;
-    nodata_token: 'OIL' | 'WATER' ;
-    data_keyword_rule: data_token (keyword_data)+;
-    data_token: 'DIMENS' | 'GRIDOPTS' | 'DRSDT' ;
-    keyword_data: '.+' ;
+    start: (section)+ 'END' ;
+    section: section_keyword section_data ;
+    section_keyword: 'RUNSPEC' | 'SCHEDULE' ;
+    @section_data: (keyword_rule)+ ;
+    @keyword_rule: nodata_keyword | data_keyword_rule ;
+    nodata_keyword: 'OIL' | 'WATER' ;
+    data_keyword_rule: data_keyword (data)+;
+    data_keyword: 'DIMENS' | 'GRIDOPTS' | 'DRSDT' ;
+    data: '.+' ;
     NEWLINE: '[\n]+' (%ignore) ;
     """)
 filename = os.path.join(os.path.dirname(__file__), 'plyplus_data1')
