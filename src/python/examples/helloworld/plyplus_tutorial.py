196396c3a40e02032778fc187109e894073f6ae5
CDN MacbookPro13 <chandan.nath@gmail.com>
2018-01-19 15:57:52 +0000

    match all chars

--- src/python/examples/helloworld/plyplus_tutorial.py
+++ src/python/examples/helloworld/plyplus_tutorial.py
@@ -11,16 +11,14 @@ parser2 = Grammar("""
     section_data: (keyword_rule)+ ;
     @keyword_rule: nodata_token | data_keyword_rule ;
     @nodata_token: 'OIL' | 'WATER' ;
-    data_keyword_rule: data_token keyword_data '/';
+    data_keyword_rule: data_token keyword_data;
     data_token: 'DIMENS' | 'GRIDOPTS' | 'DRSDT' ;
-    keyword_data: (char)+ ;
-    char: '\w' | ' ' ;
+    keyword_data: '.+' ;
     NEWLINE: '[\n]+' (%ignore) ;
     """)
 filename = os.path.join(os.path.dirname(__file__), 'plyplus_data1')
 with open(filename, "r") as f:
-    lines = f.readlines()
-    content = "\n".join(lines)
+    content = f.read()
     r2 = parser2.parse(content)
     r2_list =  [x.tail for x in r2.tail]
     print(len(r2_list))
