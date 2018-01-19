a1e9442326636ad942c180753e6c94bb10af9028
CDN MacbookPro13 <chandan.nath@gmail.com>
2018-01-19 15:01:11 +0000

    basic parser

--- src/python/examples/helloworld/plyplus_tutorial.py
+++ src/python/examples/helloworld/plyplus_tutorial.py
@@ -1,16 +1,28 @@
+import os
 from plyplus import Grammar
 
 parser1 = Grammar(r"start: name ('\s*,' name)* ; name: '\w+' ;")
 r=parser1.parse('cat ,milk,dog')
 
-
-# parser1 = Grammar(r"start: section_rule (section_rule)* ;"
-#                   r"section_rule: SECTION_KEYWORD section_data ;"
-#                   r"SECTION_KEYWORD: RUNSPEC | GRID | EDIT | PROPS | REGIONS | SOLUTION | SUMMARY | SCHEDULE | OPTIMIZE ;"
-#                   r"section_data: (keyword_rule)+ ;"
-#                   r"keyword_rule: KEYWORD_KEYWORD keyword_data ;"
-#                   r"KEYWORD_KEYWORD: DIMENS | GRIDOPTS | START | SATNUM | RPTSCHED ;"
-#                   r"keyword_data: (word)* ;"
-#                   r"word: \w+' ;")
-
-print(r)
+parser2 = Grammar("""
+    start: (section_rule)+ 'END' ;
+    section_rule: section_keyword section_data ;
+    @section_keyword: 'RUNSPEC' | 'SCHEDULE' ;
+    section_data: (keyword_rule)+ ;
+    keyword_rule: nodata_keyword | data_keyword_rule ;
+    @nodata_keyword: 'OIL' | 'WATER' ;
+    data_keyword_rule: keyword_keyword keyword_data '/';
+    @keyword_keyword: 'DIMENS' | 'GRIDOPTS' | 'DRSDT' ;
+    keyword_data: (word)* ;
+    word: '\w+' ;
+    SPACES: '[ ]+' (%ignore) ;
+    NEWLINE: '[\n]+' (%ignore) ;
+    """)
+filename = os.path.join(os.path.dirname(__file__), 'plyplus_data1')
+with open(filename, "r") as f:
+    lines = f.readlines()
+    content = "\n".join(lines)
+    r2 = parser2.parse(content)
+    print(r2)
+    r2_list =  [x.tail[0] for x in r2.tail]
+    print(r2_list)
