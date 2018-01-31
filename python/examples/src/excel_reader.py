0cb6f7b7a9b763d20d60fbab217045244c6627d1
CDN MacbookPro13 <chandan.nath@gmail.com>
2018-01-31 00:45:48 +0000

    handle ijk

--- python/examples/src/excel_reader.py
+++ python/examples/src/excel_reader.py
@@ -3,10 +3,19 @@ import pandas as pd
 
 if __name__ == '__main__':
     infile = os.path.expanduser('~/Downloads/excel.xlsx')
-    sheetno = 0
+    sheetno = 1
     df = pd.read_excel(infile, sheetno)
     cols = list(df.columns.values)
     col0 = cols[0]
     col1 = cols[1]
     for index, row in df.iterrows():
-        print("'{}': '{}',".format(row[col0].strip(), row[col1].strip()))
+        val0 = row[col0].strip()
+        val1 = row[col1].strip().replace('\n', ' ').strip()
+        val0 = val0.replace('(not E300)', '').strip()
+        if val0.endswith('(I,J,K)'):
+            val0 = val0.replace('(I,J,K)', '').strip()
+            for dir in ['I', 'J', 'K']:
+                print("'{}{}': '{} ({})',".format(val0, dir, val1, dir))
+        else:
+
+            print("'{}': '{}',".format(val0, val1))
